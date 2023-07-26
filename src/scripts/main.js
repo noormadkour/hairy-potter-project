5b28963a81f5dff968832acdf0693c3a5042735d
Noor Madkour <noormadkour@gmail.com>
2023-07-26 09:15:21 -0500

    added the check for sale function and exported to main. also did the same with the new array function that only returns items that aren't cracked and adds a price to them based on weight.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,12 @@
 // Imports go first
 import { makePottery } from "./makePottery.js";
 import { firePottery } from "./kiln.js";
+import { toSellOrNotToSell, usePottery } from "./potteryCatalog.js";
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery("Mug", 2, 5);
-let platter = makePottery("Platter", 5, 7);
-let tumbler = makePottery("Tumbler", 3, 3);
+let mug = makePottery("Mug", 7, 5);
+let platter = makePottery("Platter", 9, 7);
+let tumbler = makePottery("Tumbler", 5, 3);
 let ringHolder = makePottery("Ring Holder", 4, 6);
 let paperweight = makePottery("Paperweight", 8, 4);
 
@@ -15,11 +16,21 @@ firePottery(tumbler, 2150);
 firePottery(ringHolder, 2201);
 firePottery(paperweight, 2190);
 
-console.log(mug);
-console.log(platter);
-console.log(tumbler);
-console.log(ringHolder);
-console.log(paperweight);
+toSellOrNotToSell(mug);
+toSellOrNotToSell(platter);
+toSellOrNotToSell(tumbler);
+toSellOrNotToSell(ringHolder);
+toSellOrNotToSell(paperweight);
+
+const itemsForSale = usePottery();
+
+// console.log(mug);
+// console.log(platter);
+// console.log(tumbler);
+// console.log(ringHolder);
+// console.log(paperweight);
+
+console.log(itemsForSale)
 
 // Fire each piece of pottery in the kiln
 
---
+++ src/scripts/potteryCatalog.js
@@ -0,0 +1,15 @@
+const saleArray = [];
+
+export const toSellOrNotToSell = (clayObject) => {
+    if (clayObject.weight >= 6 && clayObject.cracked === false){
+        clayObject['price'] = 40;
+        saleArray.push(clayObject);
+    } else if (clayObject.weight < 6 && clayObject.cracked === false) {
+        clayObject['price'] = 20;
+        saleArray.push(clayObject);
+    }
+}
+
+export const usePottery = () => {
+    return saleArray
+}
